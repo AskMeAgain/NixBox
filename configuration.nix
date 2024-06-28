c57647a0932da9ccfd071b692491179e59121487
Kelvin Petry <test@test.de>
2024-06-28 15:26:08 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -4,18 +4,22 @@
     imports =
     [
         ./hardware-configuration.nix
-    ];
+    ] ++ lib.optional (builtins.fileExists /home/dev/user.nix) [ /home/dev/user.nix ];
 
     boot.loader.grub.device = "/dev/sda";
     services.openssh.enable = true;
 
     console = {
         keyMap = "de";
-    }
+    };
 
-    services.xserver.enable = true;
-    services.xserver.xkb.layout = "de";
+    services.xserver {
+        enable = true;
+        xkb.layout = "de";
+        layout = "de";
+    };
+
     users.users.dev = {
         extraGroups = [ "wheel" ];
-    }
+    };
 }
