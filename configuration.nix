2f4852a8ac5f271b30315e1d0f18a8f6b4c226f2
Kelvin Petry <test@test.de>
2024-06-28 15:38:16 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,16 @@
-{ libs, config, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-    imports =
-    [
-        ./hardware-configuration.nix
-    ] ++ lib.optional (builtins.fileExists /home/dev/user.nix) [ /home/dev/user.nix ];
+    let
+  lib = import <nixpkgs/lib>;
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+  ] ++ lib.optional (builtins.fileExists /home/dev/user.nix) [ /home/dev/user.nix ];
+
+  # Other configuration settings go here
+}
 
     boot.loader.grub.device = "/dev/sda";
     services.openssh.enable = true;
