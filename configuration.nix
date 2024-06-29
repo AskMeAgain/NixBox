14ef732fb815e10d08eb6eec8fc0ab4a8a36938c
Kelvin Petry <test@test.de>
2024-06-29 12:28:03 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -20,15 +20,18 @@
         layout = "de";
     };
 
-    users.mutableUsers = true;
-
-    users.users.root.initialHashedPassword = "$6$my5Sq4TJgI7IVyva$BjnCcQP9zEdYrtYKW7J7X7cpV5VR3jw.2zF/BR16T5506qDpB0QKyH2fEm08qt84u1xvk.tnR8I06/lF6IpnC1"; #1234
-    users.users.dev = {
-        initialHashedPassword = "$6$my5Sq4TJgI7IVyva$BjnCcQP9zEdYrtYKW7J7X7cpV5VR3jw.2zF/BR16T5506qDpB0QKyH2fEm08qt84u1xvk.tnR8I06/lF6IpnC1"; #1234
-        isNormalUser = true;
-        extraGroups = [ "wheel" ];
-        
-        home = "/home/dev";
-        shell = pkgs.bash;
+    users = {
+      mutableUsers = true;
+      users = {
+        root = {
+          initialHashedPassword = "$6$jUFVxfhcAsgEMI6q$L/hHdbPClSqJCbDuCor4vqkX8U37vJthK4dRaSqAxkcB/gXRiVvGixQXvsR.91MDUEWbNUeCEnEt2fhg9mn8m/"; #1234
+        };
+        dev = {
+          initialHashedPassword = "$6$jUFVxfhcAsgEMI6q$L/hHdbPClSqJCbDuCor4vqkX8U37vJthK4dRaSqAxkcB/gXRiVvGixQXvsR.91MDUEWbNUeCEnEt2fhg9mn8m/"; #1234
+          isNormalUser = true;
+          extraGroups = [ "wheel" ];
+          home = "/home/dev";
+        };
+      };
     };
 }
